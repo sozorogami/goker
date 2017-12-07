7aaa9c44f0232f885ce3aeee0972d0b0a688a8bb
Tyler Tape <tyler.tape@gmail.com>
2017-12-07 15:57:00 -0500

    Add fields to player struct

--- player.go
+++ player.go
@@ -2,8 +2,10 @@ package goker
 
 // Player represents a poker game participant
 type Player struct {
-	Name string
-	hand *Hand
+	Name   string
+	hand   *Hand
+	chips  int
+	status PlayerStatus
 }
 
 // GetHand assigns ownership of the provided hand to the receiver
@@ -24,8 +26,18 @@ func (p Player) String() string {
 	return p.Name
 }
 
-// NewPlayer constructs a player with a name and no hand
+// NewPlayer constructs a player with a name. All other values must be
+// initialized separately
 func NewPlayer(name string) *Player {
-	p := Player{name, nil}
+	p := Player{name, nil, 0, active}
 	return &p
 }
+
+type PlayerStatus int8
+
+const (
+	active PlayerStatus = iota
+	allIn
+	folded
+	eliminated
+)
